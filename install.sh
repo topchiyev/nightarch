21fd17f863cfa816cd4d58eabc3827b47a391a56
Pasha @ rpi-arch <topchiyev@gmail.com>
2025-12-24 16:56:14 +0400

    wip

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+export HAS_SUDO="$(sudo -n true 2>/dev/null)"
+export ARCH="$(uname -m)"
+
+IS_RPI=false
+if [[ -f /proc/device-tree/model ]] &&
+  grep -qi 'raspberry pi' /proc/device-tree/model; then
+  IS_RPI=true
+fi
+
+export IS_RPI
+
+# Must be passwordless sudoer
+if ! HAS_SUDO; then
+  echo "NightArch requires passwordless sudo"
+  exit 1
+fi
+
 chmod +x install/*.sh
 
 bash install/install-yay.sh
--- install/install-pacman-packages.sh
+++ install/install-pacman-packages.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-#must be passwordless sudoer
-
-ARCH="$(uname -m)"
-IS_RPI=false
-
-if [[ -f /proc/device-tree/model ]] &&
-  grep -qi 'raspberry pi' /proc/device-tree/model; then
-  IS_RPI=true
-fi
-
 sudo pacman -S --needed - <../packages/pacman-packages-all.txt
 
 if [[ "$ARCH" == "aarch64" ]]; then
   sudo pacman -S --needed - <../packages/pacman-packages-arm64.txt
+  if [[ IS_RPI ]]; then
+    sudo pacman -S --needed - <../packages/pacman-packages-rpi5.txt
+  fi
+else if [[ "$ARCH" == "x86_64"]]; then
+  sudo pacman -S --needed - <../packages/pacman-packages-amd64.txt
 fi
