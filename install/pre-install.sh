e0a0dca65529b4718d11877411c3e0ac77258483
Pasha @ rpi-arch <topchiyev@gmail.com>
2025-12-25 15:20:01 +0400

    wip

--- install/pre-install.sh
+++ install/pre-install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo pacman -S --needed --noconfirm iwd || true
+sudo pacman -S --needed --noconfirm iptables-nft nftables iwd || true
 sudo systemctl disable --now NetworkManager || true
 sudo pacman -R --noconfirm networkmanager || true
 sudo systemctl enable --now iwd || true
-sudo pacman -S --needed --noconfirm iptables-nft nftables
