c5ee46aafee8d6bed0067f33bce2375535d9faba
Pasha @ rpi-arch <topchiyev@gmail.com>
2025-12-25 14:46:27 +0400

    fix

--- webinstall.sh
+++ webinstall.sh
@@ -8,13 +8,13 @@ sudo pacman -S --needed --noconfirm curl unzip
 
 tmp_dir="$HOME/nightarch"
 
-zipfile="$rootdir/nightarch.zip"
+zipfile="$tmp_dir/nightarch.zip"
 
 curl -fL "https://github.com/topchiyev/nightarch/archive/refs/heads/main.zip" -o "$zipfile"
 
-unzip -q "$zipfile" -d "$tmpdir"
+unzip -q "$zipfile" -d "$tmp_dir"
 
-repo_dir="$tmpdir/nightarch-main"
+repo_dir="$tmp_dir/nightarch-main"
 
 if [[ ! -f "$repo_dir/install.sh" ]]; then
   echo "ERROR: install.sh not found in $repo_dir"
